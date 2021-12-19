af3d4268d8abaf9e1edb09a14676bc5972acfeef
Swilic <Swilick@aol.com>
2021-12-19 18:51:59 +0100

    - Demande la colonne besoin - Fait la moyenne en bas de la colonne - Demande plusieurs fois les notes - À FAIRE, colonne acquis / non acquis   + couleur si + 50%

--- main.py
+++ main.py
@@ -3,20 +3,58 @@ from openpyxl import *
 # Charge le document
 wb = load_workbook(filename="Note.xlsx")
 sheet = wb.active
-# Nom des cours sur la première ligne. AO, FR, Prog, Sys Expl, GBDD
-# Total sur la ligne 20 -- G
-# Acquis/ Non acquis sur la ligne 21 -- G
-note = input("Entrez la note: ")
-note = note.split("/")
-note_sur_vingt = int(note[0]) / int(note[1]) * 20
 
-# Cherche une case vide pour le FR || Modifier pour user choisi
-fr, a, b = "B2", 2, 0
-while sheet[fr].value is not None:
-	fr = "B" + str(a)
-	a += 1
-# Ecrit la note/20 sur un emplacement libre
-sheet[fr] = note_sur_vingt
+# Demande le cours et crée une variable pour la nommé + variable numéro colonne.
+cours = input("Quel cours: AO, FR, PY, SYS, GBD? ").lower()
+col_cours = 0
+if cours == "ao":
+	cours = "A"
+	col_cours = 1
+elif cours == "fr":
+	cours = "B"
+	col_cours = 2
+elif cours == "py":
+	cours = "C"
+	col_cours = 3
+elif cours == "sys":
+	cours = "D"
+	col_cours = 4
+elif cours == "gdb":
+	cours = "E"
+	col_cours = 5
+else:
+	print("Pas valide!")
 
+# Créer boucle pour demander note plusieurs fois.
+Continuer = True
+while Continuer:
+	# Nom des cours sur la première ligne. AO, FR, Prog, Sys Expl, GBDD
+	# Total sur la ligne 20 -- G
+	# Acquis/ Non acquis sur la ligne 21 -- G
+	note = input("Q pour quitter"+"\nEntrez la note: ")
+	# stop tout si user demande de quiter
+	if note.lower() == "q":
+		break
+	else:
+		pass
+	note = note.split("/")
+	note_sur_vingt = int(note[0]) / int(note[1]) * 20
 
-wb.save(filename="Note.xlsx")
+	# Cherche une case vide pour le FR
+	a = 2
+	colonne = cours + str(a)
+	while sheet[colonne].value is not None:
+		a += 1
+		colonne = cours + str(a)
+	# Ecrit la note/20 sur un emplacement libre
+	sheet[colonne] = note_sur_vingt
+
+	# Fais la somme de chaque valeur dans la colonne.
+	somme = 0
+	for i in sheet.iter_rows(min_row=2, max_row=a, min_col=col_cours, max_col=col_cours):
+		somme += i[0].value
+
+	# Ecrit la moyenne en bas de la colonne.
+	sheet[cours+"20"] = somme / (a-1)
+
+	wb.save(filename="Note.xlsx")
